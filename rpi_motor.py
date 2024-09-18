b36376c010da83dc2483935c0bdd35b145d1e068
billy billy <seanhung2000@gmail.com>
2024-09-18 21:04:58 +0800

    test

--- rpi_motor.py
+++ rpi_motor.py
@@ -1,7 +1,8 @@
 import time
 import board
 import busio
-from adafruit_pca9685 import PCA9685
+from Adafruit_PCA9685 import PCA9685
+
 i2c = busio.I2C(board.SCL, board.SDA)
 pca = PCA9685(i2c)
 pca.frequency = 50
