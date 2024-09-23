b5574fc3a9f5b17b3dbb290c78b30bd2173694bc
billy billy <seanhung2000@gmail.com>
2024-09-23 18:15:51 +0800

    d

--- motor_init.py
+++ motor_init.py
@@ -5,10 +5,10 @@ import time
 def init_pose(kit):
     kit.servo[1].angle = 90
     kit.servo[2].angle = 90
-    kit.servo[3].angle = 166
+    kit.servo[3].angle = 180
     kit.servo[4].angle = 90
     kit.servo[5].angle = 90
-    kit.servo[6].angle = 166
+    kit.servo[6].angle = 180
     kit.servo[7].angle = 90
     kit.servo[8].angle = 90
     kit.servo[9].angle = 14
