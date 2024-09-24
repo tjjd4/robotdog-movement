75e047b71cb8cabb9dd2f50f1eec80fbd4d372f7
tjjd4 <t01123581321345589@gmail.com>
2024-09-25 00:31:10 +0800

    add: test two leg

--- test_two_leg.py
+++ test_two_leg.py
@@ -3,34 +3,42 @@ import adafruit_motor.servo
 import time
 
 def init_pose(kit):
-    kit.servo[1].angle = 90
+    kit.servo[1].angle = 105
     kit.servo[2].angle = 90
     kit.servo[3].angle = 180
     kit.servo[4].angle = 90
     kit.servo[5].angle = 90
     kit.servo[6].angle = 180
-    kit.servo[7].angle = 90
+    kit.servo[7].angle = 85
     kit.servo[8].angle = 110
-    kit.servo[9].angle = 5
-    kit.servo[10].angle = 110
+    kit.servo[9].angle = 36
+    kit.servo[10].angle = 100
     kit.servo[11].angle = 110
-    kit.servo[12].angle = 0
+    kit.servo[12].angle = 14
 
 def rear_leg_up(kit):
-    kit.servo[5].angle += 30
-    kit.servo[11].angle -= 30
+    kit.servo[5].angle += 60
+    kit.servo[6].angle -= 20
+    kit.servo[11].angle -= 60
+    kit.servo[12].angle += 20
 
 def rear_leg_down(kit):
-    kit.servo[5].angle -= 30
-    kit.servo[11].angle += 30
+    kit.servo[5].angle -= 60
+    kit.servo[6].angle += 20
+    kit.servo[11].angle += 60
+    kit.servo[12].angle -= 20
 
 def front_leg_up(kit):
-    kit.servo[2].angle += 30
-    kit.servo[8].angle -= 30
+    kit.servo[2].angle += 60
+    kit.servo[3].angle -= 20
+    kit.servo[8].angle -= 60
+    kit.servo[9].angle += 20
 
 def front_leg_down(kit):
-    kit.servo[2].angle -= 30
-    kit.servo[8].angle += 30
+    kit.servo[2].angle -= 60
+    kit.servo[3].angle += 20
+    kit.servo[8].angle += 60
+    kit.servo[9].angle -= 20
 
 
 if __name__ == '__main__':
