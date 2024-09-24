6096816487ee8ad124349a69d6d815a753a99c3b
tjjd4 <t01123581321345589@gmail.com>
2024-09-24 23:46:34 +0800

    add: test two leg

--- test_position.py
+++ test_position.py
@@ -11,7 +11,7 @@ def init_pose(kit):
     kit.servo[6].angle = 180
     kit.servo[7].angle = 90
     kit.servo[8].angle = 110
-    kit.servo[9].angle = 14
+    kit.servo[9].angle = 36
     kit.servo[10].angle = 110
     kit.servo[11].angle = 110
     kit.servo[12].angle = 14
