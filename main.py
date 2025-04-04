e69ea3ebf302f3ac6d4ed4a5d86189ce66d0bef1
003 <robotdog003@raspberrypi>
2025-04-04 15:58:19 +0800

    ix: pdate dependency

--- main.py
+++ main.py
@@ -1,20 +1,12 @@
-import importlib
-import argparse
-
-# from controllers.local_keyboard_controller import controller
+from gateways.ServerGateway import ServerGateway
 from model.Robotdog import Robotdog
+from model.LidarController import LidarController
 
-def get_controller(controller_file):
-    controller_lib = importlib.import_module(controller_file)
-    return controller_lib.controller
+robotdog = Robotdog()
+lidar_controller = LidarController()
+
+gateway = ServerGateway(robotdog, lidar_controller)
+app = gateway.get_app()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--controller', default='controllers.static_controller')
-    args = parser.parse_args()
-
-    controller = get_controller(args.controller)
-
-    r = Robotdog()
-    r.calibrate()
-    r.move(controller)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
