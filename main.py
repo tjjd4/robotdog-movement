7038910ab9584fc33d5d03f49068437aeb8e3436
tjjd4 <003@rpi>
2025-04-16 22:56:40 +0800

    feat: check lidar controller

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from gateways.ServerGateway import ServerGateway
 from model.Robotdog import Robotdog
-from model.LidarController import LidarController
 
 robotdog = Robotdog()
-lidar_controller = LidarController()
 
-gateway = ServerGateway(robotdog, lidar_controller)
+gateway = ServerGateway(robotdog)
 app = gateway.get_app()
 
 if __name__ == "__main__":
