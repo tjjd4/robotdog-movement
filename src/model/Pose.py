de27081057ca4e91e671d1c2ca08e5272587ec1f
tjjd4 <bbb@ssstjjd4.local>
2025-05-05 13:36:59 +0800

    feat: setup simple pose (need test)

--- src/model/Pose.py
+++ src/model/Pose.py
@@ -1,26 +1,30 @@
-
-
 from src.model.custom_types.index import FootPositions, Position
+from src.utils.ConfigHelper import ConfigHelper
 
 class Pose:
+    movement_config = ConfigHelper.get_section("movement_parameters")
+    MAX_HEIGHT = movement_config.getfloat("max_height", fallback=15.0)
+    MIN_HEIGHT = movement_config.getfloat("min_height", fallback=10.0)
+    MID_HEIGHT = (MAX_HEIGHT + MIN_HEIGHT) / 2
+    
     poses = {
         "STAND": FootPositions(
-            FL=Position(x=0, y=-15, z=0),
-            FR=Position(x=0, y=-15, z=0),
-            BL=Position(x=0, y=-15, z=0),
-            BR=Position(x=0, y=-15, z=0),
+            FL=Position(x=0, y=-MAX_HEIGHT, z=0),
+            FR=Position(x=0, y=-MAX_HEIGHT, z=0),
+            BL=Position(x=0, y=-MAX_HEIGHT, z=0),
+            BR=Position(x=0, y=-MAX_HEIGHT, z=0),
         ),
         "SIT": FootPositions(
-            FL=Position(x=5, y=-15, z=5),
-            FR=Position(x=5, y=-15, z=-5),
-            BL=Position(x=-5, y=-15, z=5),
-            BR=Position(x=-5, y=-15, z=-5),
+            FL=Position(x=0, y=-MAX_HEIGHT, z=0),
+            FR=Position(x=0, y=-MAX_HEIGHT, z=0),
+            BL=Position(x=0, y=-MIN_HEIGHT, z=0),
+            BR=Position(x=0, y=-MIN_HEIGHT, z=0),
         ),
         "PEE": FootPositions(
-            FL=Position(x=5, y=-20, z=5),
-            FR=Position(x=5, y=-20, z=-5),
-            BL=Position(x=-5, y=-20, z=5),
-            BR=Position(x=-5, y=-20, z=-5),
+            FL=Position(x=0, y=-MID_HEIGHT, z=0),
+            FR=Position(x=0, y=-MID_HEIGHT, z=0),
+            BL=Position(x=0, y=-MID_HEIGHT, z=0),
+            BR=Position(x=0, y=-MID_HEIGHT, z=5),
         ),
     }
 
