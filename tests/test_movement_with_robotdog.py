01018207965ed5d915f581df90a25f4a846b0fe7
tjjd4 <bbb@ssstjjd4.local>
2025-05-11 13:23:01 +0800

    fix: remove z direction velocity

--- tests/test_movement_with_robotdog.py
+++ tests/test_movement_with_robotdog.py
@@ -12,8 +12,6 @@ def test_fixed_movements(robotdog: Robotdog):
     actions = [
         ("前進", MotionCommand(horizontal_velocity=np.array([3.0, 0.0]), yaw_rate=0.0, behavior_state=BehaviorState.MOVE)),
         ("後退", MotionCommand(horizontal_velocity=np.array([-3.0, 0.0]), yaw_rate=0.0, behavior_state=BehaviorState.MOVE)),
-        ("左移", MotionCommand(horizontal_velocity=np.array([0.0, 3.0]), yaw_rate=0.0, behavior_state=BehaviorState.MOVE)),
-        ("右移", MotionCommand(horizontal_velocity=np.array([0.0, -3.0]), yaw_rate=0.0, behavior_state=BehaviorState.MOVE)),
         ("左轉", MotionCommand(horizontal_velocity=np.array([1.0, 0.0]), yaw_rate=0.5, behavior_state=BehaviorState.MOVE)),
         ("右轉", MotionCommand(horizontal_velocity=np.array([1.0, 0.0]), yaw_rate=-0.5, behavior_state=BehaviorState.MOVE)),
         ("靜止", MotionCommand(horizontal_velocity=np.array([0.0, 0.0]), yaw_rate=0.0, behavior_state=BehaviorState.REST)),
